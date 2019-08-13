cacc523aee4142c008d939bf19382746dfe14682
kudokunn <34270106+kudokunn@users.noreply.github.com>
2019-08-13 11:29:03 +0700

    a
    a

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', name: 'HANGPT' })
+  res.render('index', { title: 'Express', name: 'HANGPTABC' })
 });
 
 /* GET sub page. */
