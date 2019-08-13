7d5f625df1aea805c240b5e8ff01f6a186493e03
kudokunn <34270106+kudokunn@users.noreply.github.com>
2019-08-13 15:39:46 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', name: 'miinhlb' })
+  res.render('index', { title: 'Express', name: 'hangpt' })
 });
 
 /* GET sub page. */
